14c206b175162c39962c8518730dd6ac11b0a1ac
jiajunwei <731264383@qq.com>
2017-11-10 11:34:34 +0800

    优化手动滚动

--- JWAdView/ViewController.m
+++ JWAdView/ViewController.m
@@ -109,7 +109,7 @@ static NSString *itemId = @"testItem";
     }
     
     CGFloat targetX = pageSize * page2;
-    return CGPointMake(targetX, offset.y);
+    return CGPointMake(targetX - 80.0, offset.y);
 }
 
 
